6c22aae4bf9287569836776b4f444ce64d25c3a2
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-22 16:45:28 -0400

    Add tests for succeed()

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { succeed, fail, repair } = require('./enhancer.js');
+
+describe("Enhancing system", () => {
+    describe("succeed()", () => {
+        it("Add 1 to enhancement", () => {
+
+            const item = {
+                enhancement: 15
+            }
+
+            const actual = succeed(item);
+
+            expect(actual.enhancement).toBe(16);
+
+        })
+        it("Does not add 1 if enhancement is 20", () => {
+
+            const item = {
+                enhancement: 20
+            }
+
+            const actual = succeed(item);
+
+            expect(actual.enhancement).toBe(20);
+        })
+    })
+})
